8b9d4d0707ed04440ca036f8a1a05846e9d184a3
dreis0 <miguel.reis@wildlifestudios.com>
2024-08-23 18:15:57 -0300

    Initial interfaces

--- application/interfaces.go
+++ application/interfaces.go
@@ -1,1 +1,19 @@
 package application
+
+type UseCases interface {
+	// Shorten receives a URL and returns a shortened version of it
+	// e.g. http://www.google.com -> abc123
+	Shorten(url string) (string, error)
+
+	// Expand receives a shortened URL and returns the original URL
+	// e.g. abc123 -> http://www.google.com
+	Expand(url string) (string, error)
+}
+
+type Dal interface {
+	// Save receives a URL and returns a shortened version of it
+	Save(shortened, url string) error
+
+	// Find receives a shortened URL and returns the original URL
+	Find(short string) (string, error)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/gmurayama/url-shortner
 
-go 1.22
+go 1.23
